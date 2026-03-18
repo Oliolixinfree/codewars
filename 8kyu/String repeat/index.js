493f791f20d261039994bb27caea404a9a06c904
Oliolixinfree <offlane@tut.by>
2026-03-18 17:15:22 +0300

    String repeat

---
+++ 8kyu/String repeat/index.js
@@ -0,0 +1,12 @@
+function repeatStr(n, s) {
+	return s.repeat(n)
+}
+
+console.log(repeatStr(3, '*'))
+console.log(repeatStr(5, '#'))
+console.log(repeatStr(2, 'ha '))
+console.log(repeatStr(0, ''))
+console.log(repeatStr(0, 'I'))
+console.log(repeatStr(5, ''))
+console.log(repeatStr(6, 'I'))
+console.log(repeatStr(5, 'Hello'))
