90b32926dca3160e27dc9d28d9fc9b89ef3afe46
Oliolixinfree <offlane@tut.by>
2026-03-18 15:39:58 +0300

    Return Negative

---
+++ 8kyu/Return Negative/index.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+	return num < 0 ? num : num * -1
+}
+
+console.log(makeNegative(42))
+console.log(makeNegative(0))
+console.log(makeNegative(0.19))
+console.log(makeNegative(-0.19))
