7f91f0e2fc40cc3d8964ccac2ad94809b1afb0eb
Oliolixinfree <offlane@tut.by>
2026-03-20 12:33:50 +0300

    Sum of odd numbers

---
+++ 7kyu/Sum of odd numbers/index.js
@@ -0,0 +1,12 @@
+function rowSumOddNumbers(n) {
+	return n ** 2 * n
+}
+
+// or
+
+function rowSumOddNumbers(n) {
+	return n ** 3
+}
+
+console.log(rowSumOddNumbers(1))
+console.log(rowSumOddNumbers(42))
